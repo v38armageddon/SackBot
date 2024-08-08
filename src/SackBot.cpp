a7427e70b87fdb2244cd26ccb97e04e7b5ab018e
v38armageddon <florian.m@v38armageddon.net>
2024-08-08 20:08:55 +0200

    [HELP] Create help command

--- src/SackBot.cpp
+++ src/SackBot.cpp
@@ -28,7 +28,27 @@ int main()
 	// Handle slash command with the most recent addition to D++ features, coroutines!
 	bot.on_slashcommand([](const dpp::slashcommand_t& event) -> dpp::task<void> {
 		if (event.command.get_command_name() == "joshua") {
-			co_await event.co_reply("Hello, v38armageddon.!");
+			co_await event.co_reply("Hello, v38armageddon.");
+		}
+		if (event.command.get_command_name() == "help") {
+			dpp::embed embed = dpp::embed()
+				.set_color(dpp::colors::dark_blue)
+				.set_title("Help for SackBot")
+				.set_author("v38armageddon", "https://github.com/v38armageddon/SackBot", "https://cdn.discordapp.com/avatars/1270782508036001895/8f6e97bbe782e2da4a5b5319da553106.webp?size=128")
+				.set_description("Here you can find all commands for SackBot.")
+				.set_image("https://cdn.discordapp.com/avatars/1270782508036001895/8f6e97bbe782e2da4a5b5319da553106.webp?size=128")
+				.add_field("joshua", "A simple test command", true)
+				.set_footer(
+					dpp::embed_footer()
+					.set_text("SackBot is created by v38armageddon | v0.2")
+					.set_icon("https://cdn.discordapp.com/avatars/1270782508036001895/8f6e97bbe782e2da4a5b5319da553106.webp?size=128")
+				)
+				.set_timestamp(time(0));
+
+			// Create the message with the embed
+			dpp::message msg = dpp::message(event.command.channel_id, embed);
+
+			event.reply(msg);
 		}
 		co_return;
 	});
